25b1bd73b24b412403b7a4b62d34c147812a89a5
Vighnesh Radhakrishnan <vighnesh7radhakrishnan@gmail.com>
2025-11-10 15:50:13 +0530

    added working copy of pyFAST

--- frontend/src/components/Fruits.jsx
+++ frontend/src/components/Fruits.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from "../../../api.js";
+import api from "../api.js";
 import AddFruitForm from "./AddFruitForm.jsx";
 
 const FruitList = () => {
